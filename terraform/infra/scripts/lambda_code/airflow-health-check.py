1fd5161f72be40ee9d287a1c5a4a38253ba72904
cicerojmm@gmail.com <cicerojmm@gmail.com>
2025-12-04 09:13:20 -0300

    melhorias nos codigo da sfn e lambda

--- terraform/infra/scripts/lambda_code/airflow-health-check.py
+++ terraform/infra/scripts/lambda_code/airflow-health-check.py
@@ -6,7 +6,7 @@ def lambda_handler(event, context):
     # URL do Airflow como variável de ambiente
     airflow_host = os.environ.get('AIRFLOW_HOST', 'ec2-13-58-137-11.us-east-2.compute.amazonaws.com')
     airflow_port = os.environ.get('AIRFLOW_PORT', '8080')
-    url = f"http://{airflow_host}:{airflow_port}/health"
+    url = f"http://{airflow_host}:{airflow_port}/api/v2/monitor/health"
 
     try:
         with urllib.request.urlopen(url) as response:
